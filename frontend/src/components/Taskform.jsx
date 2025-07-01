ec9a79d707d9012dcdfc3b86c23cee3232f9f7ad
rohitmahajan200 <rm90087@gmail.com>
2025-07-01 19:07:00 +0530

    beutify code structure

--- frontend/src/components/Taskform.jsx
+++ frontend/src/components/Taskform.jsx
@@ -17,17 +17,40 @@ const dispatch=useDispatch();
         e.preventDefault();
         const task={title,description,assignto,status};
         dispatch(addTask(task));
-        // await axios.post("http://localhost:5000/",task);
     }
 
   return (
     <>    
-    <form className='flex flex-col justify-center items-center gap-5' onSubmit={()=>handleSubmit()}>
+    <form className='flex flex-col justify-center items-center gap-5' onSubmit={handleSubmit}>
         <div>Create-Task</div>
-        <input placeholder='Title' name='title' onChange={(e)=>setTitle(e.target.value)} className='border-2 border-blue-300 w-70 h-10 rounded-xs'></input>
-        <input placeholder='Discription' name='description' onChange={(e)=>setDescription(e.target.value)} className='border-2 border-blue-300 h-10 w-70 rounded-xs'></input>
-        <input placeholder='Assing To' name='assignto' onChange={(e)=>setAssignto(e.target.value)} className='border-2 border-blue-300 h-10 w-70 rounded-xs'></input>
-        <input placeholder='Status' name='status' onChange={(e)=>setStatus(e.target.value)} className='border-2 border-blue-300 h-10 w-70 rounded-xs'></input>
+        <input placeholder='Title'
+         name='title'
+         value={title}
+         onChange={(e)=>setTitle(e.target.value)} 
+         className='border-2 border-blue-300 w-70 h-10 rounded-xs'>
+         </input>
+
+        <input placeholder='Discription' 
+        name='description' 
+        value={description}
+        onChange={(e)=>setDescription(e.target.value)} 
+        className='border-2 border-blue-300 h-10 w-70 rounded-xs'>
+        </input>
+
+        <input placeholder='Assing To'
+         name='assignto' 
+         value={assignto}
+         onChange={(e)=>setAssignto(e.target.value)} 
+         className='border-2 border-blue-300 h-10 w-70 rounded-xs'>
+         </input>
+
+        <input placeholder='Status' 
+        name='status' 
+        value={status}
+        onChange={(e)=>setStatus(e.target.value)} 
+        className='border-2 border-blue-300 h-10 w-70 rounded-xs'>
+        </input>
+
         <button className='bg-blue-400 text-black w-30 rounded-xl hover:cursor-pointer'>Create Task</button>
         <Link className='text-white hover:text-blue-300 italic' to="/tasks">Checkout-Assingn Tasks</Link>
     </form>
