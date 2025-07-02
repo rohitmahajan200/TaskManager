d87df00d43e770626b6d75ca39f4455e149fa273
rohitmahajan200 <rm90087@gmail.com>
2025-07-02 18:13:24 +0530

    add cors for production

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({extended:true}));
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://task-manager-u3jc.vercel.app/',
   credentials:true
 }));
 
