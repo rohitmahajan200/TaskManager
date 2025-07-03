9b59077b4a2ed2b5c3c3e1525827748a2e7b3d5e
rohitmahajan200 <rm90087@gmail.com>
2025-07-03 18:37:22 +0530

    cookie issue on prod

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -25,7 +25,7 @@ const state=useSelector((state)=>state.user);
             },2000)
             loginAlert("Login Successfully!");
         }else{
-          loginAlert(state?.error);
+          loginAlert(state?.error?.message);
         }      
     },[dispatch,state])
 
