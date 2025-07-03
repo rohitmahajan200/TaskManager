b77bf27c09722d42f46b7142064167aa263ead72
rohitmahajan200 <rm90087@gmail.com>
2025-07-03 18:06:13 +0530

    state bug fix

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -19,14 +19,19 @@ const state=useSelector((state)=>state.user);
     }
 
     useEffect(()=>{
-      if(state.user.user.success){
-            setTimeout(()=>{
-              navigate("/tasks");
-            },2000)
-            loginAlert("Login Successfully!");
-        }else{
-          loginAlert(state.user.message);
-        }
+      // if(state.user.user.success){
+      //       setTimeout(()=>{
+      //         navigate("/tasks");
+      //       },2000)
+      //       loginAlert("Login Successfully!");
+      //   }else{
+      //     loginAlert(state.user.message);
+      //   }
+      console.log("state",state);
+      console.log("state?.user",state?.user);
+      console.log("state?.user?.mesaage",state?.user?.mesaage);
+      console.log("state?.user?.success",state?.user?.success);
+      
     },[dispatch,state])
 
 
